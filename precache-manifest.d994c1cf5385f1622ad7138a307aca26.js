a0c7db5aea058dc78e272c8ad8fdf8d5077d066a
youzung <damaskpop@naver.com>
2020-01-20 12:44:24 +0900

    Updates

---
+++ precache-manifest.d994c1cf5385f1622ad7138a307aca26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbcc306267fe2fd554dc4cd9a4df8a9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "650b455cec7676672e81",
+    "url": "/movie_app/static/css/main.e4d026c4.chunk.css"
+  },
+  {
+    "revision": "aeac4f46517183faa72c",
+    "url": "/movie_app/static/js/2.90848efc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.90848efc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "650b455cec7676672e81",
+    "url": "/movie_app/static/js/main.eed2de6d.chunk.js"
+  },
+  {
+    "revision": "ded0cf15c95271782aca",
+    "url": "/movie_app/static/js/runtime-main.dd8fea41.js"
+  }
+]);
